2a871451980a098611926df9379cd3fd09aa40fe
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-15 22:45:59 -0800

    Time: 320 ms (87.38%) | Memory: 0B (100.00%) - LeetSync

--- 1749-sellers-with-no-sales/sellers-with-no-sales.sql
+++ 1749-sellers-with-no-sales/sellers-with-no-sales.sql
@@ -1,7 +1,8 @@
 -- Write your PostgreSQL query statement below
 SELECT s.seller_name
 FROM Seller s
-LEFT JOIN orders o 
-ON s.seller_id = o.seller_id AND EXTRACT(YEAR FROM o.sale_date) = 2020
+LEFT JOIN (SELECT seller_id,order_id FROM orders
+            WHERE EXTRACT(YEAR FROM sale_date)=2020)o
+USING(seller_id)
 WHERE o.order_id IS NULL 
 ORDER BY s.seller_name ASC;
