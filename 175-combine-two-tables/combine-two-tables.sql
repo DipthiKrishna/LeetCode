66ab30b37d2288396584b61beff43664afb49b4d
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-15 22:56:01 -0800

    Time: 412 ms (75.80%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,5 +1,5 @@
-# Write your MySQL query statement below
-SELECT p.firstname,p.lastname,a.city,a.state
-FROM person p
-LEFT JOIN address a
-ON p.personid=a.personid;
+-- Write your PostgreSQL query statement below
+SELECT firstname,lastname,city,state
+FROM person 
+LEFT JOIN address
+ON person.personid = address.personid;
