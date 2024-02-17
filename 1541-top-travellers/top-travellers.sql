f089cc7bf5909d1b51da1c187fb161cd7701a0e0
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-16 21:16:55 -0800

    Time: 479 ms (82.33%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT u.name,
+        COALESCE(SUM(r.distance),0) AS travelled_distance
+FROM Users u
+LEFT JOIN Rides r ON u.id=r.user_id
+GROUP BY r.user_id,u.name
+ORDER BY travelled_distance DESC, u.name ASC;
