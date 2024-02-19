665b110d8ef0c37ea9c399c41fe66006783fca3f
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-19 12:32:36 -0800

    Time: 1969 ms (25.74%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -2,9 +2,9 @@
 
 SELECT e.left_operand,e.operator,e.right_operand,
 CASE WHEN e.operator = '>' THEN CASE WHEN v1.value > v2.value THEN 'true' ELSE 'false' END 
-     WHEN e.operator = '<' THEN CASE WHEN v1.value < v2.value THEN 'true' ELSE 'false' END
-     WHEN e.operator = '=' THEN CASE WHEN v1.value = v2.value THEN 'true' ELSE 'false' END
-     END AS value
+WHEN e.operator = '<' THEN CASE WHEN v1.value < v2.value THEN 'true' ELSE 'false' END
+WHEN e.operator = '=' THEN CASE WHEN v1.value = v2.value THEN 'true' ELSE 'false' END
+END AS value
 FROM expressions e
 JOIN Variables v1 ON e.left_operand=v1.name 
 JOIN VAriables v2 ON e.right_operand=v2.name;
