618ac78bf740ba8172ff0ad4ac4e5dd927c4b652
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-19 12:34:26 -0800

    Time: 1255 ms (72.28%) | Memory: 0B (100.00%) - LeetSync

--- 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
+++ 1565-evaluate-boolean-expression/evaluate-boolean-expression.sql
@@ -1,9 +1,10 @@
 -- Write your PostgreSQL query statement below
 
 SELECT e.left_operand,e.operator,e.right_operand,
-CASE WHEN e.operator = '>' THEN CASE WHEN v1.value > v2.value THEN 'true' ELSE 'false' END 
-WHEN e.operator = '<' THEN CASE WHEN v1.value < v2.value THEN 'true' ELSE 'false' END
-WHEN e.operator = '=' THEN CASE WHEN v1.value = v2.value THEN 'true' ELSE 'false' END
+CASE WHEN e.operator = '>'  AND v1.value > v2.value THEN 'true' 
+WHEN e.operator = '<' AND v1.value < v2.value THEN 'true'
+WHEN e.operator = '=' AND v1.value = v2.value THEN 'true'
+ELSE 'false'
 END AS value
 FROM expressions e
 JOIN Variables v1 ON e.left_operand=v1.name 
