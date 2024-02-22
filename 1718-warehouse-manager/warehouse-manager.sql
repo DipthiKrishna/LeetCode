b25fd133d4d36e91a4425223c553ecdd17ec3cdc
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-22 11:38:38 -0800

    Time: 952 ms (6.74%) | Memory: 0B (100.00%) - LeetSync

--- 1718-warehouse-manager/warehouse-manager.sql
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -2,5 +2,5 @@
 select name as warehouse_name,SUM(units*width*length*height) as volume
 from warehouse
 left join products
-on warehouse.product_id=products.product_id
+using(product_id)
 group by name;
