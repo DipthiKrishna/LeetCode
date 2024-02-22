ca5160c28e221d0dc36f602ecca1512a9f4b3a6d
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-22 11:39:19 -0800

    Time: 547 ms (33.71%) | Memory: 0B (100.00%) - LeetSync

--- 1718-warehouse-manager/warehouse-manager.sql
+++ 1718-warehouse-manager/warehouse-manager.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
-select name as warehouse_name,SUM(units*width*length*height) as volume
+select warehouse.name as warehouse_name,SUM(units*width*products.length*height) as volume
 from warehouse
 left join products
 using(product_id)
-group by name;
+group by warehouse.name;
