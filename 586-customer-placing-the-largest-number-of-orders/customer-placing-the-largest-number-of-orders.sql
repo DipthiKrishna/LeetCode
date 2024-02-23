846e6eeaa8319bb68355a39a07afac3844f05bab
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-23 12:01:46 -0800

    Time: 529 ms (53.26%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -2,8 +2,5 @@
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-HAVING COUNT(order_number) IN (SELECT COUNT(order_number)
-                            FROM Orders 
-                            GROUP BY customer_number
-                            ORDER BY COUNT(order_number) DESC
-                            LIMIT 1);
+ORDER BY COUNT(order_number) DESC
+LIMIT 1;
