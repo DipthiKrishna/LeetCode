ab45fb1ed18b059654a733732ebe1a4605b7a0cf
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-24 23:22:55 -0800

    Time: 656 ms (71.43%) | Memory: 0B (100.00%) - LeetSync

--- 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
+++ 1291-immediate-food-delivery-i/immediate-food-delivery-i.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
 
-SELECT ROUND((SELECT COUNT(delivery_id) FROM Delivery
-WHERE order_date=customer_pref_delivery_date)*100.0/COUNT(*),2) AS immediate_percentage 
+SELECT ROUND((SELECT COUNT(delivery_id)*100.0 FROM Delivery
+WHERE order_date=customer_pref_delivery_date)/COUNT(*),2) AS immediate_percentage 
 FROM delivery;
