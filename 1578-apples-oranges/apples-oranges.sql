77102a990da0edeee8582cdba30972bc31f8f4c7
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-02-25 22:03:14 -0800

    Time: 612 ms (23.50%) | Memory: 0B (100.00%) - LeetSync

--- 1578-apples-oranges/apples-oranges.sql
+++ 1578-apples-oranges/apples-oranges.sql
@@ -1,12 +1,14 @@
 -- Write your PostgreSQL query statement below
 WITH ap AS
-(SELECT sale_date,sold_num
+(SELECT sale_date,SUM(sold_num) AS sold_num
 FROM Sales
-WHERE fruit = 'apples'),
+WHERE fruit = 'apples'
+GROUP BY sale_date),
 os AS
-(SELECT sale_date,sold_num
+(SELECT sale_date,SUM(sold_num) AS sold_num
 FROM Sales
-WHERE fruit = 'oranges')
+WHERE fruit = 'oranges'
+GROUP BY sale_date)
 SELECT ap.sale_date,(ap.sold_num - os.sold_num)AS diff
 FROM ap
 JOIN os ON ap.sale_date = os.sale_date
