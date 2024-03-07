60f9eb952ad919a974da4e6703b49ab55dabde18
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-06 22:12:46 -0800

    Time: 300 ms (97.03%) | Memory: 0B (100.00%) - LeetSync

--- 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
+++ 1639-friendly-movies-streamed-last-month/friendly-movies-streamed-last-month.sql
@@ -2,8 +2,6 @@
 SELECT DISTINCT content.title
 FROM tvprogram
 LEFT JOIN content ON tvprogram.content_id = content.content_id
-WHERE EXTRACT(year from tvprogram.program_date) = 2020
-AND EXTRACT(month from program_date) = 6
-AND content.kids_content = 'Y'
+WHERE TO_CHAR(program_date,'YYYY-MM')='2020-06' AND content.kids_content = 'Y'
 AND content.content_type = 'Movies';
 
