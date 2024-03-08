4d4a04c38e15cf4285283a03fb0b9fb2cc6ebb39
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-07 22:12:42 -0800

    Time: 959 ms (38.56%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,6 +1,4 @@
 -- Write your PostgreSQL query statement below
---WITH code AS (SELECT substring(phone_number,1,3) AS country_code,id,name,phone_number
---FROM person)
 SELECT country.name AS country
 FROM country
 LEFT JOIN person ON country.country_code=substring(person.phone_number,1,3)
