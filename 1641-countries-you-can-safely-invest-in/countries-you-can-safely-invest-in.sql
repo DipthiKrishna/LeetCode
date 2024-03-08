368972a4d13fc64c9f1a67808a86754aa0a3a476
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-07 22:17:27 -0800

    Time: 1264 ms (12.42%) | Memory: 0B (100.00%) - LeetSync

--- 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
+++ 1641-countries-you-can-safely-invest-in/countries-you-can-safely-invest-in.sql
@@ -1,7 +1,10 @@
 -- Write your PostgreSQL query statement below
-SELECT country.name AS country FROM country
-LEFT JOIN person ON country.country_code=substring(person.phone_number,1,3)
-LEFT JOIN calls ON person.id IN (calls.caller_id,calls.callee_id)
+WITH code AS (SELECT substring(phone_number,1,3) AS country_code,id,name,phone_number
+FROM person)
+SELECT country.name AS country
+FROM country
+LEFT JOIN code ON country.country_code=code.country_code
+LEFT JOIN calls ON code.id IN (calls.caller_id,calls.callee_id)
 GROUP BY country.name HAVING AVG(calls.duration) > (SELECT AVG(duration) FROM calls);
 
         
