8ce01cd19985c68423cb247202dc28df28830b47
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-11 11:30:24 -0700

    Time: 625 ms (16.67%) | Memory: 0B (100.00%) - LeetSync

---
+++ 603-consecutive-available-seats/consecutive-available-seats.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT seat_id
+FROM Cinema
+WHERE free=1 AND (seat_id+1 IN (SELECT seat_id FROM cinema WHERE free=1)
+OR seat_id -1 IN (SELECT seat_id FROM cinema WHERE free=1))
+ORDER BY seat_id;
