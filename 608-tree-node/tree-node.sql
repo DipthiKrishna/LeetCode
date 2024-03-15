eddb50b9dcacb99056305a2696338d0fd7de9921
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-14 19:29:04 -0700

    Time: 476 ms (54.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 608-tree-node/tree-node.sql
@@ -0,0 +1,6 @@
+-- Write your PostgreSQL query statement below
+SELECT id,
+CASE WHEN p_id IS NULL THEN 'Root' 
+    WHEN p_id IN (SELECT id FROM tree) AND id IN (SELECT p_id FROM tree) THEN 'Inner'
+    ELSE 'Leaf' END AS type
+FROM Tree;
