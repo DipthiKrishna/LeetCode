ca915a1d5cb8ef820c72a6f69791d5c8f6838b4e
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-16 18:19:31 -0700

    Time: 462 ms (69.05%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT DISTINCT d.name AS Department,
+e.name as Employee,
+e.salary as Salary
+FROM employee e
+INNER JOIN department d ON e.departmentId = d.id
+WHERE (d.id, e.salary) IN (SELECT departmentid,MAX(salary) FROM employee GROUP BY departmentid);
