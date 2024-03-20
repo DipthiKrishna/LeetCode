06ad296a08f595f50b73e7d94718bed5f4c7b403
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-19 22:56:13 -0700

    Time: 752 ms (28.24%) | Memory: 0B (100.00%) - LeetSync

--- 569-median-employee-salary/median-employee-salary.sql
+++ 569-median-employee-salary/median-employee-salary.sql
@@ -1,12 +1,12 @@
 -- Write your PostgreSQL query statement below
 WITH employee1 AS(
-    SELECT id,company,
+    SELECT id,company,salary,
     ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary ASC, id ASC) AS row_asc,
     ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary DESC, id DESC) AS row_desc
     FROM employee
     )
-SELECT e1.id,e1.company,e.salary
-FROM employee1 e1 JOIN employee e USING(id)
+SELECT id,company,salary
+FROM employee1
 WHERE row_asc BETWEEN row_desc-1 AND row_desc+1
-ORDER BY e1.company,e.salary
+ORDER BY company,salary
 
