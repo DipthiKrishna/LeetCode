bb4ca6714b86c73466444e607517959629969a25
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-19 23:01:05 -0700

    Time: 1180 ms (9.42%) | Memory: 0B (100.00%) - LeetSync

--- 569-median-employee-salary/median-employee-salary.sql
+++ 569-median-employee-salary/median-employee-salary.sql
@@ -1,12 +1,12 @@
 -- Write your PostgreSQL query statement below
 WITH employee1 AS(
     SELECT id,company,salary,
-    ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary ASC, id ASC) AS row_asc,
-    ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary DESC, id DESC) AS row_desc
+    ROW_NUMBER() OVER(PARTITION BY company ORDER BY salary) AS rank,
+    COUNT(*) OVER(PARTITION BY company) AS com_count
     FROM employee
     )
 SELECT id,company,salary
 FROM employee1
-WHERE row_asc BETWEEN row_desc-1 AND row_desc+1
-ORDER BY company,salary
+WHERE rank BETWEEN com_count/2::decimal AND (com_count/2)+1::decimal
+ORDER BY company,salary;
 
