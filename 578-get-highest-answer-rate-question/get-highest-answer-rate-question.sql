3e3eea8ff658d1945f8ff85de16e1f297fab3150
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-22 19:06:52 -0700

    Time: 472 ms (52.14%) | Memory: 0B (100.00%) - LeetSync

--- 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
+++ 578-get-highest-answer-rate-question/get-highest-answer-rate-question.sql
@@ -1,9 +1,9 @@
 WITH a1 AS (SELECT question_id,
-SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)*1.0 / SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS answer_rate
+SUM(CASE WHEN action = 'answer' THEN 1 ELSE 0 END)*1.0 / SUM(CASE WHEN action = 'show' THEN 1 ELSE 0 END) AS rate
 FROM SurveyLog
 GROUP BY question_id)
 SELECT question_id AS survey_log
-FROM (SELECT question_id,RANK() OVER (ORDER BY answer_rate DESC,question_id ASC) AS rnk
+FROM (SELECT question_id,RANK() OVER(ORDER BY rate DESC,question_id ASC) AS rnk
 FROM a1) AS rk
 WHERE rnk = 1;
 
