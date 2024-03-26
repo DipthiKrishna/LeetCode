44c2c76a2237383bcb93df91a3096add8008b0df
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-26 12:29:13 -0700

    Time: 453 ms (58.45%) | Memory: 0B (100.00%) - LeetSync

--- 579-find-cumulative-salary-of-an-employee/find-cumulative-salary-of-an-employee.sql
+++ 579-find-cumulative-salary-of-an-employee/find-cumulative-salary-of-an-employee.sql
@@ -1,6 +1,4 @@
-SELECT id,month,
-SUM(salary)OVER(PARTITION BY id ORDER BY month
-RANGE BETWEEN 2 PRECEDING AND CURRENT ROW) As salary
+SELECT id,month,SUM(salary)OVER(PARTITION BY id ORDER BY month RANGE BETWEEN 2 PRECEDING AND CURRENT ROW) As salary
 FROM employee
 WHERE (id,month) NOT IN (SELECT id,MAX(month) as month FROM employee GROUP BY id)
 ORDER BY id, month DESC;
