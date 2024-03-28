ea07d6dc4a67ad0c515fce5313b14685f0a709bd
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-28 00:03:42 -0700

    Time: 636 ms (25.37%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -4,7 +4,7 @@ c1 AS (SELECT COUNT(DISTINCT CONCAT(sender_id, send_to_id)) AS nr
 FROM FriendRequest)
 SELECT ROUND(
     CASE 
-        WHEN c1.nr = 0 THEN 0 
-        ELSE CAST(c2.na * 1.0 / c1.nr AS DECIMAL)
-    END ,2)AS accept_rate
-FROM c2, c1;
+        WHEN c1.nr <> 0 THEN c2.na/c1.nr::numeric 
+        ELSE 0
+    END
+,2)AS accept_rate FROM c2, c1;
