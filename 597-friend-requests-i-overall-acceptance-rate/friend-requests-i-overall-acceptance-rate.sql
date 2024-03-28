989c59d8703e2a0ac88d77dd8e30e352471594c0
Deepthi Binu <130187997+DipthiKrishna@users.noreply.github.com>
2024-03-27 23:59:14 -0700

    Time: 745 ms (21.64%) | Memory: 0B (100.00%) - LeetSync

--- 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
+++ 597-friend-requests-i-overall-acceptance-rate/friend-requests-i-overall-acceptance-rate.sql
@@ -1,15 +1,7 @@
-/*WITH c2 AS (select count(distinct(concat(requester_id,accepter_id))) as na FROM requestaccepted),
-c1 AS(select COUNT(distinct(concat(sender_id,send_to_id)))as nr FROM FriendRequest)
-SELECT case when c1.nr =0.00 then 0 else cast(c2.na*1.0/c1.nr as decimal) end aS accept_rate;
-from c2, c1;*/
-WITH c2 AS (
-    SELECT COUNT(DISTINCT CONCAT(requester_id, accepter_id)) AS na 
-    FROM requestaccepted
-),
-c1 AS (
-    SELECT COUNT(DISTINCT CONCAT(sender_id, send_to_id)) AS nr 
-    FROM FriendRequest
-)
+WITH c2 AS (SELECT COUNT(DISTINCT CONCAT(requester_id, accepter_id)) AS na
+FROM requestaccepted ),
+c1 AS (SELECT COUNT(DISTINCT CONCAT(sender_id, send_to_id)) AS nr
+FROM FriendRequest)
 SELECT ROUND(
     CASE 
         WHEN c1.nr = 0 THEN 0 
